f507f6080f4b124694ccba5c5f980d2bcbf5070d
developbiao <developbiao@gmail.com>
2022-08-21 16:27:09 +0800

    add integrated gin

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import "C"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello Air!")
+	// Initialization gin instance
+	r := gin.Default()
+
+	// Registration middleware
+	r.Use(gin.Logger(), gin.Recovery())
+
+	// Registration route
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data": "Hello Gin is working!",
+		})
+	})
+
+	// Processing 404 request
+	r.NoRoute(func(c *gin.Context) {
+		// Get Accept value from header
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "Page not found 404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "Page not found 404",
+			})
+		}
+	})
+
+	// Running server on 8000 port
+	r.Run(":8000")
 }
