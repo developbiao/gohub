f606ed103384e41cacb970c497335d906b8680b9
developbiao <developbiao@gmail.com>
2023-03-20 16:17:41 +0800

    cache friendly link data

--- app/models/link/link_util.go
+++ app/models/link/link_util.go
@@ -1,40 +1,63 @@
 package link
 
 import (
-    "gohub/pkg/database"
-    "gohub/pkg/app"
-    "gohub/pkg/paginator"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"gohub/pkg/app"
+	"gohub/pkg/cache"
+	"gohub/pkg/database"
+	"gohub/pkg/helpers"
+	"gohub/pkg/paginator"
+	"time"
 )
 
 func Get(idstr string) (link Link) {
-    database.DB.Where("id", idstr).First(&link)
-    return
+	database.DB.Where("id", idstr).First(&link)
+	return
 }
 
 func GetBy(field, value string) (link Link) {
-    database.DB.Where("? = ?", field, value).First(&link)
-    return
+	database.DB.Where("? = ?", field, value).First(&link)
+	return
 }
 
 func All() (links []Link) {
-    database.DB.Find(&links)
-    return
+	database.DB.Find(&links)
+	return
 }
 
 func IsExist(field, value string) bool {
-    var count int64
-    database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
-    return count > 0
+	var count int64
+	database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
+	return count > 0
 }
 
 func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Paging) {
-    paging = paginator.Paginate(
-        c,
-        database.DB.Model(Link{}),
-        &links,
-        app.V1URL(database.TableName(&Link{})),
-        perPage,
-    )
-    return
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(Link{}),
+		&links,
+		app.V1URL(database.TableName(&Link{})),
+		perPage,
+	)
+	return
+}
+
+func AllCached() (links []Link) {
+	//Set cache key
+	cacheKey := "links:all"
+	// Set cache time
+	expireTime := 120 * time.Minute
+	// Get data
+	cache.GetObject(cacheKey, &links)
+
+	// If not hit cache query from database
+	if helpers.Empty(links) {
+		// Query database
+		links = All()
+		if helpers.Empty(links) {
+			return
+		}
+		cache.Set(cacheKey, links, expireTime)
+	}
+	return
 }
--- bootstrap/cache.go
+++ bootstrap/cache.go
@@ -9,7 +9,7 @@ import (
 func SetupCache() {
 	// Initialization the cache-specific redis client, use the dedicated cache DB
 	rds := cache.NewRedisStore(
-		fmt.Sprintf("%v,%v", config.GetString("redis.host"), config.GetString("redis.port")),
+		fmt.Sprintf("%v:%v", config.GetString("redis.host"), config.GetString("redis.port")),
 		config.GetString("redis.username"),
 		config.GetString("redis.password"),
 		config.GetInt("redis.database_cache"),
